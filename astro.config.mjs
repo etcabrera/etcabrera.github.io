7f08dd9c8e6c1c71bdb78d00774110fd8084ed1d
Tomás Cabrera <e.tomas.cabrera@gmail.com>
2025-05-20 17:46:28 -0300

    Adicion de idiomas y correccion de colores.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,8 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://etcabrera.github.io',
+    i18n: {
+    defaultLocale: "es",
+    locales: ["es", "en", "it"],
+  }
 });
